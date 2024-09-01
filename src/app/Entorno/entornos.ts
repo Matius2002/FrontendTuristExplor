e5f09a0a8fc052f1e9b5f61c7bdb60f1f7f55a3b
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-09-01 06:46:51 -0500

    Se comentarea entornos.ts

--- src/app/Entorno/entornos.ts
+++ src/app/Entorno/entornos.ts
@@ -1,7 +1,9 @@
-export const entornos =
-  {
-    production: false,
+// Exporta un objeto de configuración llamado `entornos`
+export const entornos = {
+  production: false, // Indica si el entorno es de producción; `false` significa que es un entorno de desarrollo o pruebas
 
-    dynamicHost: 'localhost:8080', // Desarrollo
-    //dynamicHost: '10.100.207.13:8080' // Pruebas falta modificarla
-  };
+  // Dirección del host dinámico para las solicitudes HTTP en el entorno de desarrollo
+  dynamicHost: 'localhost:8080', // Desarrollo, utilizado para ejecutar la aplicación localmente
+
+  // dynamicHost: '10.100.207.13:8080' // Pruebas, comentado y pendiente de modificación para entornos de prueba
+};
