51e4a61271cd0e3f0833745134d7d085f0bc7b11
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-09-01 06:58:58 -0500

    Se comentarea TouristSite.ts

--- src/app/modelos/TouristSite.ts
+++ src/app/modelos/TouristSite.ts
@@ -1,8 +1,9 @@
+// Exporta la interfaz `TouristSite` que define la estructura de un sitio turístico
 export interface TouristSite {
-  id?: number;
-  title: string;
-  content: string;
-  tourismType: string;
-  latitude: number;
-  longitude: number;
+  id?: number; // Identificador único del sitio turístico (opcional)
+  title: string; // Título o nombre del sitio turístico
+  content: string; // Descripción o contenido sobre el sitio turístico
+  tourismType: string; // Tipo de turismo asociado al sitio (por ejemplo, cultural, aventura)
+  latitude: number; // Coordenada de latitud del sitio, utilizada para posicionarlo en un mapa
+  longitude: number; // Coordenada de longitud del sitio, utilizada para posicionarlo en un mapa
 }
