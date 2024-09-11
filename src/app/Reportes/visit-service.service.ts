b0e52e16106ae426c0f5416f677909107c2cc887
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-09-11 08:08:05 -0500

    Se gaurdan cambios

--- src/app/Reportes/visit-service.service.ts
+++ src/app/Reportes/visit-service.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import {entornos} from "../Entorno/entornos";
-import {Observable} from "rxjs";
-import {HttpClient} from "@angular/common/http";
+import { entornos } from "../Entorno/entornos";
+import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -9,10 +9,27 @@ import {HttpClient} from "@angular/common/http";
 export class VisitService {
   dynamicHost = entornos.dynamicHost;
   private baseUrl: string = `http://${this.dynamicHost}/api/visitas`;
+
   constructor(private http: HttpClient) {}
+
+  // Método para registrar una visita
   registerVisit(route: string): Observable<any> {
     return this.http.post(this.baseUrl, { route });
   }
+
+  // Método para descargar el reporte en formato Excel
+  downloadReportExcel(): Observable<Blob> {
+    const url = `${this.baseUrl}/reporte/excel`; // Asegúrate que esta ruta esté configurada en el backend
+    return this.http.get(url, {
+      responseType: 'blob', // Indica que la respuesta será un archivo binario (Blob)
+    });
+  }
+
+  // Método para descargar el reporte en formato PDF
+  downloadReportPDF(): Observable<Blob> {
+    const url = `${this.baseUrl}/reporte/pdf`; // Asegúrate que esta ruta esté configurada en el backend
+    return this.http.get(url, {
+      responseType: 'blob', // Indica que la respuesta será un archivo binario (Blob)
+    });
+  }
 }
-//ya anterior mete me habias dado la idea de como hacer lo que es esta ayudame a completar ese reporte
-// recuerda que es en excel y pdf:
