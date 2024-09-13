3924d3640e217d4b0ba22c5e059f25889481dde1
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-09-13 16:16:14 -0500

    Front hoy viernes 13/09/2024

--- src/app/Turismo/Experiencia/nueva-experiencia/nueva-experiencia.component.ts
+++ src/app/Turismo/Experiencia/nueva-experiencia/nueva-experiencia.component.ts
@@ -1,161 +1,151 @@
-// Importaciones necesarias para el componente y su funcionamiento
-import { Component, OnInit } from '@angular/core'; // Importa Component para definir el componente y OnInit para manejar la inicialización
-import { HttpClient, HttpClientModule } from "@angular/common/http"; // Proporciona funcionalidad HTTP para interactuar con APIs
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms"; // Maneja formularios reactivos y sus validaciones
-import { NgClass, NgForOf, NgIf } from "@angular/common"; // Directivas de Angular para condicionales, bucles y manejo de clases CSS
-import { ExperienciaService } from "../experiencia.service"; // Servicio personalizado para manejar la lógica de negocio de experiencias
-import { Router } from "@angular/router"; // Permite la navegación entre rutas de la aplicación
-import Swal from "sweetalert2"; // Librería para mostrar alertas y notificaciones
-import { UsuarioService } from "../../../Admin/Usuarios/usuario.service"; // Servicio para manejar los usuarios
+import { Component, OnInit } from '@angular/core'; 
+import { HttpClient, HttpClientModule } from "@angular/common/http"; 
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms"; 
+import { NgClass, NgForOf, NgIf } from "@angular/common"; 
+import { ExperienciaService } from "../experiencia.service"; 
+import { Router } from "@angular/router"; 
+import Swal from "sweetalert2"; 
+import { UsuarioService } from "../../../Admin/Usuarios/usuario.service"; 
 
-// Objeto Usuario que define la estructura esperada de un usuario
+
+//Objeto Usuario
 interface Usuario {
-  id: number; // Identificador único del usuario
-  nombreUsuario: string; // Nombre de usuario
-  email: string; // Correo electrónico del usuario
+  id: number; 
+  nombreUsuario: string; 
+  email: string; 
 }
 
-// Objeto Destinos que define la estructura esperada de un destino
+// Objeto Destinos
 interface Destinos {
-  id: number; // Identificador único del destino
-  destinoName: string; // Nombre del destino
+  id: number; 
+  destinoName: string; 
 }
 
-// Objeto Experiencia que define la estructura esperada de una experiencia
+// Objeto Experiencia
 interface Experiencia {
-  id: number; // Identificador único de la experiencia
-  calificacion: string; // Calificación de la experiencia
-  comentario: string; // Comentario del usuario sobre la experiencia
-  fecha: string; // Fecha en la que se registra la experiencia
-  usuario: Usuario; // Objeto usuario que contiene los datos del usuario asociado a la experiencia
-  destinos: Destinos; // Objeto destinos que contiene los datos del destino asociado a la experiencia
+  id: number; 
+  calificacion: string; 
+  comentario: string; 
+  fecha: string; 
+  usuario: Usuario; 
+  destinos: Destinos; 
 }
 
-// Decorador Component que define el comportamiento y metadatos del componente
 @Component({
-  providers: [ExperienciaService, HttpClient, UsuarioService], // Proveedores de servicios que estarán disponibles en este componente
-  selector: 'app-nueva-experiencia', // Nombre del selector del componente para usarlo en otras plantillas
-  standalone: true, // Indica que este componente es independiente y no depende de un módulo Angular tradicional
-  imports: [ // Lista de módulos y directivas importadas que se utilizarán dentro del componente
-    FormsModule, // Módulo para trabajar con formularios básicos
-    NgIf, // Directiva para condicionales en el HTML
-    ReactiveFormsModule, // Módulo para manejar formularios reactivos
-    NgClass, // Directiva para manipulación dinámica de clases CSS
-    NgForOf, // Directiva para iterar sobre listas en el HTML
-    HttpClientModule // Módulo para realizar peticiones HTTP
+  providers: [ExperienciaService, HttpClient, UsuarioService], 
+  selector: 'app-nueva-experiencia', 
+  standalone: true, 
+  imports: [ 
+    FormsModule, 
+    NgIf, 
+    ReactiveFormsModule, 
+    NgClass, 
+    NgForOf, 
+    HttpClientModule 
   ],
-  templateUrl: './nueva-experiencia.component.html', // Ruta al archivo de plantilla HTML del componente
-  styleUrl: './nueva-experiencia.component.css' // Ruta al archivo de estilos CSS del componente
+  templateUrl: './nueva-experiencia.component.html', 
+  styleUrl: './nueva-experiencia.component.css' 
 })
 
-// Clase NuevaExperienciaComponent que define la lógica del componente
-export class NuevaExperienciaComponent implements OnInit {
-  crearForm!: FormGroup; // FormGroup para manejar el formulario reactivo
-  experiencias!: Experiencia; // Objeto de tipo Experiencia para almacenar datos de la experiencia actual
-  usuarios: Usuario[] = []; // Arreglo de usuarios disponibles
-  destinos: Destinos[] = []; // Arreglo de destinos disponibles
-  isSubmitting: boolean = false; // Indicador para manejar el estado de envío del formulario
-  currentUser: Usuario | null = null; // Objeto para almacenar el usuario actual autenticado
-  fecha: any; // Variable para manejar la fecha (a definir su uso)
-  comentario: any; // Variable para manejar comentarios (a definir su uso)
-  calificacion: any; // Variable para manejar calificaciones (a definir su uso)
 
-  // Constructor del componente, inyectando los servicios y dependencias necesarias
+export class NuevaExperienciaComponent implements OnInit {
+  crearForm!: FormGroup; 
+  experiencias!: Experiencia; 
+  usuarios: Usuario[] = [];
+  destinos: Destinos[] = []; 
+  isSubmitting: boolean = false; 
+  currentUser: Usuario | null = null; 
+  fecha: any; 
+  comentario: any; 
+  calificacion: any; 
+
   constructor(
-    private formBuilder: FormBuilder, // Servicio para construir formularios
-    private experienciaService: ExperienciaService, // Servicio para manejar las experiencias
-    private router: Router, // Servicio de Angular para la navegación entre rutas
-    private usuariosService: UsuarioService, // Servicio para manejar la lógica de usuarios
+    private formBuilder: FormBuilder, 
+    private experienciaService: ExperienciaService, 
+    private router: Router,
+    private usuariosService: UsuarioService, 
   ) { }
 
-  // Método que se ejecuta al inicializar el componente
   ngOnInit(): void {
-    // Verificar si el usuario está autenticado al iniciar el componente
-    this.currentUser = this.usuariosService.getCurrentUser(); // Obtiene el usuario actual autenticado
-    if (!this.currentUser) { // Si no hay usuario autenticado
-      Swal.fire('Usuario no autenticado', 'Debe iniciar sesión para registrar una experiencia.', 'warning') // Muestra un mensaje de advertencia
+    this.currentUser = this.usuariosService.getCurrentUser(); 
+    
+    
+    if (!this.currentUser) { 
+      Swal.fire('Usuario no autenticado', 'Debe iniciar sesión para registrar una experiencia.', 'warning') 
         .then(() => {
         });
-      return; // Detiene la ejecución del componente si no está autenticado
+      return; 
     }
 
-    // Inicializa el formulario reactivo con sus campos y validaciones
     this.crearForm = this.formBuilder.group({
-      comentario: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(500)]], // Campo para comentario con validaciones
-      destinos: ['', Validators.required], // Campo para el destino requerido
-      calificacion: ['', Validators.required] // Campo para la calificación requerida
+      comentario: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(500)]], 
+      destinos: ['', Validators.required], 
+      calificacion: ['', Validators.required] 
     });
-    this.cargarDestinos(); // Carga los destinos disponibles llamando a un método del servicio
+    this.cargarDestinos(); 
   }
 
-  // Método para cargar los destinos disponibles desde el servicio
+  
   cargarDestinos(): void {
     this.experienciaService.recuperarTodosDestinos().subscribe(
-      (destinos: Destinos[]) => { // Al recibir los destinos desde el backend
-        this.destinos = destinos; // Asigna los destinos recibidos al arreglo local
+      (destinos: Destinos[]) => { 
+        this.destinos = destinos; 
       },
-      (error) => { // Manejo de errores si ocurre un fallo en la solicitud
-        console.error(error); // Imprime el error en consola
+      (error) => { 
+        console.error(error); 
       }
     );
   }
 
-  // Método que se ejecuta al enviar el formulario
   onSubmit(): void {
-    // Verificar si el usuario está autenticado
-    if (!this.currentUser) { // Si no hay usuario autenticado
-      Swal.fire('Usuario no autenticado', 'Debe iniciar sesión para guardar una experiencia.', 'error'); // Muestra un mensaje de error
-      this.router.navigate(['/login']); // Redirige al usuario a la página de inicio de sesión
-      return; // Detiene la ejecución del método
+    if (!this.currentUser) { 
+      Swal.fire('Usuario no autenticado', 'Debe iniciar sesión para guardar una experiencia.', 'error'); 
+      this.router.navigate(['/login']); 
+      return; 
     }
 
-
-    // Validar el formulario
-    if (this.crearForm.invalid) { // Si el formulario es inválido
-      Swal.fire('Error', 'Por favor complete el formulario correctamente.', 'error'); // Muestra un mensaje de error
-      return; // Detiene la ejecución del método
+    if (this.crearForm.invalid) { 
+      Swal.fire('Error', 'Por favor complete el formulario correctamente.', 'error'); 
+      return; 
     }
 
-    // Capturar los valores del formulario
-    const destinoSeleccionado = this.crearForm.get('destinos')?.value; // Obtiene el valor del campo de destino
+    
+    const destinoSeleccionado = this.crearForm.get('destinos')?.value; 
     console.log('Destino seleccionado antes de enviar:', destinoSeleccionado);
-    if (!destinoSeleccionado || !destinoSeleccionado.id) { // Verifica que el destino seleccionado tenga un ID válido
-      Swal.fire('Error', 'El ID del destino seleccionado no es válido o está vacío.', 'error'); // Muestra un mensaje de error
-      return; // Detiene la ejecución del método
+    if (!destinoSeleccionado || !destinoSeleccionado.id) { 
+      Swal.fire('Error', 'El ID del destino seleccionado no es válido o está vacío.', 'error'); 
+      return; 
     }
 
-    // Construir el objeto de la experiencia
     const experiencia: Experiencia = {
-      id: 0, // Se asigna 0, ya que se espera que el backend genere un ID único
-      calificacion: this.crearForm.value.calificacion, // Asigna la calificación del formulario
-      comentario: this.crearForm.value.comentario, // Asigna el comentario del formulario
-      fecha: new Date().toISOString(), // Asigna la fecha actual en formato ISO
-      usuario: this.currentUser, // Asigna el usuario autenticado */
-      destinos: destinoSeleccionado, // Asigna el destino seleccionado
+      id: 0, 
+      calificacion: this.crearForm.value.calificacion, 
+      comentario: this.crearForm.value.comentario, 
+      fecha: new Date().toISOString(), 
+      usuario: this.currentUser, 
+      destinos: destinoSeleccionado, 
     };
 
     console.log('Datos que se enviarán: ', experiencia);
 
-    // Enviar la experiencia al backend
+    
     this.experienciaService.guardarExperiencia(experiencia).subscribe({
       next: (response) => {
         console.log('Experiencia guardada exitosamente', response);
-        // Lógica en caso de éxito
+        
       },
       error: (error) => {
-        console.error('Error al guardar la experiencia:', error);
-        console.error('Código de error:', error.status, 'Mensaje:', error.message);
+        console.error('Error al guardar la experiencia: ', error); //Error
       }
     });
   }
 
-  // Método para limpiar el formulario
   limpiarFormulario() {
-    this.crearForm.reset(); // Resetea los campos del formulario a su estado inicial
+    this.crearForm.reset(); 
   }
 
-  // Método para navegar a la ruta inicial
+  
   volver() {
-    this.router.navigate(['/tu-inicio']); // Navega a la página de inicio
+    this.router.navigate(['/tu-inicio']); 
   }
 }
