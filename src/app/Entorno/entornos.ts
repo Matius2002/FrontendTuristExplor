42762a86f527e80f8240c12afff1dc2ab01e4f05
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-09-26 14:42:55 -0500

    Se configura el front para produccion

--- src/app/Entorno/entornos.ts
+++ src/app/Entorno/entornos.ts
@@ -3,7 +3,7 @@ export const entornos = {
   production: false, // Indica si el entorno es de producción; `false` significa que es un entorno de desarrollo o pruebas
 
   // Dirección del host dinámico para las solicitudes HTTP en el entorno de desarrollo
-  dynamicHost: 'localhost:8080', // Desarrollo, utilizado para ejecutar la aplicación localmente
+  //dynamicHost: 'localhost:8080', // Desarrollo, utilizado para ejecutar la aplicación localmente
 
-  // dynamicHost: '10.100.207.13:8080' // Pruebas, comentado y pendiente de modificación para entornos de prueba
+   dynamicHost: 'http://ec2-3-141-3-181.us-east-2.compute.amazonaws.com:8080/' // Pruebas, comentado y pendiente de modificación para entornos de prueba
 };
